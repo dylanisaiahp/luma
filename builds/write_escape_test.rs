76f6c0ab97f0d2c61d800bb5b809d5e10e0ae279
Amethyst <dylanisaiahp@gmail.com>
2026-04-10 10:53:17 -0400

    Remove builds/ folder

--- builds/write_escape_test.rs
+++
@@ -1,9 +0,0 @@
-#![allow(unused_mut, unused_variables, unused_must_use)]
-mod luma_runtime;
-use luma_runtime::*;
-
-fn main() {
-    { luma_write(&Value::String("Hello\nWorld\\r\n".to_string())); Value::Void };
-    { luma_write(&Value::String("Tab:\there\\r\nDone".to_string())); Value::Void };
-}
-
